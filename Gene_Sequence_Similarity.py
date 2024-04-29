76277ebf08a58ebbb2592782452907c864c74e4d
mk307 <115074350+mk307@users.noreply.github.com>
2024-04-29 18:56:06 +0500

    Update Gene_Sequence_Similarity.py

--- Gene_Sequence_Similarity.py
+++ Gene_Sequence_Similarity.py
@@ -1,11 +1,16 @@
 from Bio import Align
 from Bio import SeqIO
+
 aligner = Align.PairwiseAligner()
+
 aligner.mode = 'local'
 aligner.match_score = 1.0
 aligner.mismatch_score = -2.0
 aligner.gap_score = -2.5
+
 seq1 = SeqIO.read("CRBN.fasta", "fasta")
 seq2 = SeqIO.read("IL6.fasta", "fasta")
+
 score = aligner.score(seq1.seq, seq2.seq)
+
 print(score)
